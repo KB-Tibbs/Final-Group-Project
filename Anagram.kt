2874ac93faec390302bcaad063985b689e5ae2fa
KB-Tibbs <kebur17@morgan.edu>
2022-12-08 05:01:23 -0500

    Create Anagram.kt

---
+++ Anagram.kt
@@ -0,0 +1,26 @@
+package com.example.anagrams
+
+import com.example.anagrams.Anagram
+import java.util.*
+
+class Anagram{
+    val RANDOM = Random()
+    val wrds = arrayOf("ORANGE", "BELTWAY", "APPLE", "KOTLIN", "MOJA")
+
+    public final fun ranWrd(): String {
+        return wrds[RANDOM.nextInt(wrds.size)]
+    }
+
+    fun mixWrds(anag: String?): String? {
+        if (anag != null && "" != anag) {
+            val let = anag.toCharArray()
+            for (i in let.indices) {
+                val r = RANDOM.nextInt(let.size)
+                val temp = let[i]
+                let[i] = let[r]
+                let[r] = temp
+            }
+        }
+        return anag
+    }
+}
